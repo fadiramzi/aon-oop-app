fe654056dbc3eeeb95c9ba6c53a9991162f91bb0
fadiramzi <fadiramzi.99@gmail.com>
2024-10-11 13:01:51 +0300

    New Changes

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@
 Freelancer f1 = new Freelancer();// f is instance/object
 //Freelancer f2 = new Freelancer("Fadi","fadi99");
 //Freelancer f3 = new Freelancer(1,"Fadi", "fadi99","pass");
-
 f1.Name = "Saif";
 f1.DisplayProfile();
-//f2.DisplayProfile();
 
-//f3.DisplayProfile();
+//End
+
 
 
 
